658e810b5402185d3278e6e938c44ab098d3e3fa
Ryan Angela <ryan.c.angela@gmail.com>
2021-12-09 23:59:17 -0500

    push back original quotes files

---
+++ src/modal.js
@@ -0,0 +1,26 @@
+// Modal One
+  // Get the modal
+var modal = document.getElementById("modalOne");
+
+// Get the button that opens the modal
+var btn = document.getElementById("button-one");
+
+// Get the <span> element that close-ones the modal
+var span = document.getElementsByClassName("close-one")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close-one the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close-one it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
